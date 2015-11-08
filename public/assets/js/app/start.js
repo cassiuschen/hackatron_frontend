7290510bd6fcfb4fcbaa3046b66ff3c0a2c29cbd
cassiuschen <chzsh1995@gmail.com>
2015-11-08 09:11:41 +0800

    test

--- public/assets/js/app/start.js
+++ public/assets/js/app/start.js
@@ -13,21 +13,18 @@ define(['jquery', 'wechatSetup', 'hammer'], function($, wx, Hammer){
     $(this).css('background-image','/assets/images/wave.gif');
     console.log("press");
   });
-  mic.on('pressup', function() {
-    window.wechat.stopRecord({
-        success: function (res) {
-            recordId = res.localId;
-        }
-    });
-    console.log('pressup');
-    $(this).css('opacity','1');
-    $('.main-wave').css('opacity', '0');
-    wechat.translateVoice({
-       localId: recordId, // 需要识别的音频的本地Id，由录音相关接口获得
-        isShowProgressTips: 1, // 默认为1，显示进度提示
-        success: function (res) {
-            alert(res.translateResult); // 语音识别的结果
-        }
-    });
+
+  wechat.onVoiceRecordEnd({
+      // 录音时间超过一分钟没有停止的时候会执行 complete 回调
+    complete: function (res) {
+        recordId = res.localId;
+        wechat.translateVoice({
+           localId: recordId, // 需要识别的音频的本地Id，由录音相关接口获得
+            isShowProgressTips: 1, // 默认为1，显示进度提示
+            success: function (res) {
+                alert(res.translateResult); // 语音识别的结果
+            }
+        });
+      }
   });
 });
