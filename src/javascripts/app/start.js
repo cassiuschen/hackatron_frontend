8a5b1780f8d79d2c4cc2a67000a04d861269494e
cassiuschen <chzsh1995@gmail.com>
2015-11-08 11:21:29 +0800

    fuk

--- src/javascripts/app/start.js
+++ src/javascripts/app/start.js
@@ -40,7 +40,7 @@ define(['jquery', 'wechatSetup'], function($, wx){
     setTimeout(function(){
       window.wechat.translateVoice({
        localId: recordId, // 需要识别的音频的本地Id，由录音相关接口获得
-        isShowProgressTips: 1, // 默认为1，显示进度提示
+        isShowProgressTips: 0, // 默认为1，显示进度提示
         success: function (res) {
             alert(res.translateResult); // 语音识别的结果
         }
